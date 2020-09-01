915a7703b97c66484e6e316bc88bf78edffaa4b9
Danpowers24 <danpowers610@gmail.com>
2020-09-01 12:49:52 -0700

    Add GET and POST routes for Contacts

--- models/Contact.js
+++ models/Contact.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 const ContactSchema = mongoose.Schema({
-
     user: {
-        type: mongoose.SchemaType.types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'users'
     },
     name: {
